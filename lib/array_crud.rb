348c3aba7b86de3156dc5ca444b8d04168790f00
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-09 01:03:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  ["I'm", "a", "dinosaur," "RAWR!"]
+  [ ]
 end
 
 def create_an_array
-  
+  ["I'm", "a", "dinosaur," "RAWR!"]
 end
 
 def add_element_to_end_of_array(array, element)
